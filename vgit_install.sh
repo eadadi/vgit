8978aa2665403f0ac73df09eb100f62c9c9aadaa
eadadi <eadadi@amazon.com>
2023-04-20 15:06:00 +0300

    modify pyinstaller instalation

--- vgit_install.sh
+++ vgit_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install typer[all] pyyaml GitPython
-pip install pyinstaller
+pip install -U pyinstaller
 echo "Invoking pyinstaller vgit_cli.py..."
 pyinstaller vgit_cli.py -n vg
 echo "add ./dist/vg/vg to your PATH, for example:\n\tln -s \`pwd\`/dist/vg/vg /bin/vg"
